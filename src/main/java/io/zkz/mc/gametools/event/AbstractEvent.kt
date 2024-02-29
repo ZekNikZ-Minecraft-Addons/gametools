0f314a07ea7f6e4ea84795dc4ea03de4c9ba1429
Matthew McCaskill <mattrmccaskill@gmail.com>
2024-02-29 14:08:31 -0600

    Fix loading errors on 1.20.4

--- src/main/java/io/zkz/mc/gametools/event/AbstractEvent.kt
+++ src/main/java/io/zkz/mc/gametools/event/AbstractEvent.kt
@@ -7,9 +7,14 @@ import kotlin.reflect.KClass
 abstract class AbstractEvent : Event() {
     companion object {
         private val handlerLists = mutableMapOf<KClass<*>, HandlerList>()
+
+        @JvmStatic
+        fun getHandlerList(): HandlerList {
+            return handlerLists.getOrPut(this::class, ::HandlerList)
+        }
     }
 
     override fun getHandlers(): HandlerList {
-        return handlerLists.getOrPut(this::class, ::HandlerList)
+        return getHandlerList()
     }
 }
